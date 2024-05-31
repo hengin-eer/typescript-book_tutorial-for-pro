ca58a3531841d0d7b044500266522f09b29be465
hengin-eer <hirokix0608@gmail.com>
2024-05-31 17:56:56 +0900

    ✨feat: TypeScriptのサンプルプルグラムを追加
    入力した数字をコンソールに3回出力する

---
+++ chapter-1/ts/repeatHello.js
@@ -0,0 +1,5 @@
+function repeatHello(count) {
+    return "hello".repeat(count);
+}
+var message = repeatHello(3);
+console.log(message);
---
+++ chapter-1/ts/repeatHello.ts
@@ -0,0 +1,7 @@
+function repeatHello(count: number): string {
+    return "hello".repeat(count);
+}
+
+const message: string = repeatHello(3);
+
+console.log(message);
