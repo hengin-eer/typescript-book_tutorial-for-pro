491a3dcf62eeb0578d24b2bef215cb361dd092ae
hengin-eer <hirokix0608@gmail.com>
2024-06-24 14:05:12 +0900

    🚚 move: ファイルのフォルダ名・位置を変更

--- chapter-1/ts/repeatHello.js
+++
@@ -1,5 +0,0 @@
-function repeatHello(count) {
-    return "hello".repeat(count);
-}
-var message = repeatHello(3);
-console.log(message);
---
+++ dist/repeatHello.js
@@ -0,0 +1,6 @@
+"use strict";
+function repeatHello(count) {
+    return "hello".repeat(count);
+}
+const message = repeatHello(3);
+console.log(message);
